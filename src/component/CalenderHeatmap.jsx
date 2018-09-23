6e9ac1356064a2f9688ec0ed9c3be0007c6606fa
Jing Wait <lovedoodle@users.noreply.github.com>
2018-09-23 14:53:30 -0700

    call backend endpoint to get the weather data and transform data into an array of object with property of date and count range from 1-4

--- src/component/CalenderHeatmap.jsx
+++ src/component/CalenderHeatmap.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import moment from 'moment';
+import axios from 'axios'
 
 export default class CalenderHeatmap extends Component {
     constructor(props){
@@ -17,7 +18,33 @@ export default class CalenderHeatmap extends Component {
         while (currentDay.month() === choosen_month) {
             days.push(moment(currentDay))
             currentDay = moment(currentDay).add(1, 'days');
-        } 
+        }
+        var data_defs = days.map(day => {
+            var unix_time = day.unix();
+            return axios.get(`/get_weather_data/${unix_time}`).then((c)=> {return c.data});
+        })
+        Promise.all(data_defs).then(array_of_daily_weather =>{
+            console.log(array_of_daily_weather)
+            //format an array of daily ac and heat on data 
+            var result = array_of_daily_weather.map(weather => {
+                var r = new Object();
+                r.date = moment(weather.daily.data[0].time).format('yyyy-mm-dd')
+                if (weather.daily.data[0].temperatureHigh > 75 && weather.daily.data[0].temperatureLow < 62) {
+                    r.count = 4
+                } else if (weather.daily.data[0].temperatureHigh > 75 && weather.daily.data[0].temperatureLow >= 62) {
+                    r.count = 3
+                } else if (weather.daily.data[0].temperatureHigh <= 75 && weather.daily.data[0].temperatureLow < 62) {
+                    r.count = 2
+                } else {
+                    r.count =1;
+                }
+                return r
+            })
+            this.setState({
+                data: result
+            })
+            console.log(this.state)
+        })
     }
     render() {
         return (<div>

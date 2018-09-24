25e4ba12f1e167de9f0647c5d75bee88a1a14684
Jing Wait <lovedoodle@users.noreply.github.com>
2018-09-23 18:01:43 -0700

    use react-calendar-heatmap to display data

--- src/component/CalenderHeatmap.jsx
+++ src/component/CalenderHeatmap.jsx
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import moment from 'moment';
-import axios from 'axios'
+import axios from 'axios';
+import ReactCalendarHeatmap from 'react-calendar-heatmap';
+import 'react-calendar-heatmap/dist/styles.css';
+import ReactTooltip from 'react-tooltip';
 
 export default class CalenderHeatmap extends Component {
     constructor(props){
@@ -24,32 +27,71 @@ export default class CalenderHeatmap extends Component {
             return axios.get(`/get_weather_data/${unix_time}`).then((c)=> {return c.data});
         })
         Promise.all(data_defs).then(array_of_daily_weather =>{
-            console.log(array_of_daily_weather)
-            //format an array of daily ac and heat on data 
+            //format an array of daily ac and heat on objects 
+            //for example {date: "2018-05-01", count: 2}
+            //range from 1-4, 1 when either ac/heat on, 2 when heat on, 3 when ac on, 4 both heat and ac on 
             var result = array_of_daily_weather.map(weather => {
-                var r = new Object();
-                r.date = moment(weather.daily.data[0].time).format('yyyy-mm-dd')
+                var r = {};
+                r.date = moment.unix(weather.daily.data[0].time).format('YYYY-MM-DD')
                 if (weather.daily.data[0].temperatureHigh > 75 && weather.daily.data[0].temperatureLow < 62) {
-                    r.count = 4
-                } else if (weather.daily.data[0].temperatureHigh > 75 && weather.daily.data[0].temperatureLow >= 62) {
                     r.count = 3
-                } else if (weather.daily.data[0].temperatureHigh <= 75 && weather.daily.data[0].temperatureLow < 62) {
+                } else if (weather.daily.data[0].temperatureHigh > 75 && weather.daily.data[0].temperatureLow >= 62) {
                     r.count = 2
+                } else if (weather.daily.data[0].temperatureHigh <= 75 && weather.daily.data[0].temperatureLow < 62) {
+                    r.count = 1
                 } else {
-                    r.count =1;
+                    r.count = null;
                 }
                 return r
-            })
+            });
+
             this.setState({
                 data: result
             })
-            console.log(this.state)
+            console.log(this.state.data);
         })
     }
     render() {
-        return (<div>
-            Hello World
-            
+        return (<div className="heat-map">
+            {this.state.data.length > 0 ? (
+                <div>
+                    <ReactCalendarHeatmap
+                        startDate={new Date('2018-04-30')}
+                        endDate={new Date('2018-05-31')}
+                        values={this.state.data}
+                        classForValue={(value) => {
+                            if (!value) {
+                                console.log('found empty', value)
+                                return 'color-empty';
+                            }
+                            return `color-scale-${value.count}`;
+                        }}
+                        tooltipDataAttrs={value => {
+                            var result;
+                            if(value.count === 3) {
+                                result = 'AC/Heat On'
+                            }else if (value.count === 2) {
+                                result = 'AC On'
+                            }else if(value.count === 1) {
+                                result = 'Heat On'
+                            }else {
+                                result ='AC/Heat Off'
+                            }
+                            return {
+                                'data-tip': `${value.date}: ${
+                                    result
+                                    }`,
+                            };
+                        }}
+                        showWeekdayLabels={true}
+                        horizontal={false}
+                    />
+                    <ReactTooltip />
+                </div>                
+            ) : (
+                <div>Loading</div>
+            )} 
+            <div className='color-icon green'></div>AC/Heat on<div className='color-icon red'></div>Heat on<div className='color-icon blue'></div>AC on
         </div>)
     }
 }

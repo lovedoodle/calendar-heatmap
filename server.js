84c536c2ae654ea5602b0ef8a8a1c93e3b5f97b0
Jing Wait <lovedoodle@users.noreply.github.com>
2018-09-26 16:22:51 -0700

    use redis to cache dark sky request

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+const redis = require('redis');
+const redisClient = redis.createClient(process.env.REDIS_URL);
+
+// Make all redis commands promises 
+const bluebird = require('bluebird');
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
+
+
 const path = require('path');
 var express = require('express')
 var app = express();
@@ -9,9 +18,19 @@ app.use('/build', express.static('build'));
 
 app.get('/get_weather_data/:unix_time', function (req, res) {
     var unix_time = req.params.unix_time; //CHANGE
-    axios.get(`https://api.darksky.net/forecast/${process.env.REACT_APP_DARK_SKY_API_KEY}/45.5898,-122.5951,${unix_time}?exclude=currently,flags,hourly`).then((data_response) => {
-        res.send(data_response.data);
-    });
+    var redis_key =  `dark-sky-cache-${unix_time}`;
+    redisClient.getAsync(redis_key).then(weatherData => {
+        if(weatherData){
+            console.log('cached data', unix_time)
+            res.send(JSON.parse(weatherData))
+        }else {
+            axios.get(`https://api.darksky.net/forecast/${process.env.REACT_APP_DARK_SKY_API_KEY}/45.5898,-122.5951,${unix_time}?exclude=currently,flags,hourly`).then((data_response) => {
+                console.log('calling this external api')
+                redisClient.set(redis_key, JSON.stringify(data_response.data))
+                res.send(data_response.data);
+            });
+        }
+    })
 })
 
 app.get('/*', function (req, res) {
